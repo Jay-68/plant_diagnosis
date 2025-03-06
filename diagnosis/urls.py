62d68dc6cdd188c74a805a18663be3f7e9cea121
Jay-68 <ngari.james.n@gmail.com>
2025-03-06 06:15:47 +0300

    f around and find out

--- diagnosis/urls.py
+++ diagnosis/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import disease_search, index
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name='home'),
     path('search/', disease_search, name='disease_search'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
