e0069e3ffb193741d4c80a107ed64cb4b4b466a2
Jay-68 <ngari.james.n@gmail.com>
2025-03-07 20:25:12 +0300

    Why are you searching for?

--- diagnosis/views.py
+++ diagnosis/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from .models import PlantDisease
+from django.db.models import Q
 
 
 def disease_search(request):
     query = request.GET.get('symptoms', '')
     diseases = PlantDisease.objects.filter(
-        symptoms_icontains=query) if query else None
+        Q(symptoms__icontains=query) |
+        Q(name__icontains=query)
+    ) if query else None
     return render(request, 'diagnosis/search.html', {'diseases': diseases, 'query': query})
 
 
